c37ebc6094543869857c5e380293050db77d4306
yisheng wang <468539746@qq.com>
2022-09-03 00:02:27 +0800

    crate project frame

---  backend/kob/src/main/java/com/qxty/kob/controller/pk/IndexController.java
+++  backend/kob/src/main/java/com/qxty/kob/controller/pk/IndexController.java
@@ -1,9 +1,21 @@
 package com.qxty.kob.controller.pk;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping(/pk)
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/pk")
 public class IndexController {
+    @RequestMapping("/getbotinfo")
+    public Map<String, String> getBotInfo()
+    {
+        Map<String, String> bot1 = new HashMap<>();
+        bot1.put("name", "rot1");
+        bot1.put("rating", "1500");
+        return bot1;
+    }
+
 }
