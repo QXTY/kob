a03cd079bc0c8064bda313c29e399cd5bda970ad
yisheng wang <468539746@qq.com>
2022-09-16 20:58:06 +0800

    finish snake

--- front/src/assets/js/GameMap.js
+++ front/src/assets/js/GameMap.js
@@ -1,5 +1,6 @@
 import { GameObject } from "./GameObject";
 import { Wall } from "./Wall";
+import { Snake } from './Snake';
 
 export class GameMap extends GameObject {
     constructor(ctx, parent) {
@@ -8,10 +9,15 @@ export class GameMap extends GameObject {
         this.parent = parent;
         this.L = 0;
         this.rows = 13;
-        this.cols = 13;
+        this.cols = 14;
 
         this.walls = [];
         this.inner_walls_count = 20;
+
+        this.snakes = [
+            new Snake({id: 0, color: "#48761C", r: this.rows - 2, c: 1},this),
+            new Snake({id: 1, color: "#F94848", r: 1, c: this.cols - 2},this),
+        ];
     }
     check_connectivity(g, sx, sy, tx, ty) {
         if (sx == tx && sy == ty) return true;
@@ -51,9 +57,9 @@ export class GameMap extends GameObject {
             for (let j = 0; j < 1000; j ++ ){
                 let r = parseInt(Math.random()* this.rows);
                 let c = parseInt(Math.random()* this.cols);
-                if (g[r][c] || g[c][r]) continue;
+                if (g[r][c] || g[this.rows - 1 - r][this.cols - 1 - c]) continue;
                 if (r == this.rows - 2 && c == 1 || r == 1 && this.cols - 2) continue;
-                g[r][c] = g[c][r] = true;
+                g[r][c] = g[this.rows - 1 - r][this.cols - 1 - c] = true;
                 break;
             }
         }
@@ -74,6 +80,8 @@ export class GameMap extends GameObject {
     start() {
         for (let i = 0; i < 1000; i ++ )
         if (this.creat_walls()) break;
+
+        this.add_listrning_events();
          
     }
     updated_size() {
@@ -81,9 +89,58 @@ export class GameMap extends GameObject {
         this.ctx.canvas.width = this.L * this.cols;
         this.ctx.canvas.height = this.L * this.rows;
     }
+    check_ready() { //判断两条蛇是否准备好下一回合的移动
+        for (const snake of this.snakes) {
+            if (snake.status !== "idle") return false;
+            if (snake.direction === -1) return false;
+        }
+        return true;
+    }
+    add_listrning_events() {
+        this.ctx.canvas.focus();
+        const [snake0, snake1] = this.snakes;
+
+        this.ctx.canvas.addEventListener("keydown", e => {
+            if (e.key === 'w') snake0.set_direction(0);
+            else if (e.key === 'd') snake0.set_direction(1);
+            else if (e.key === 's') snake0.set_direction(2);
+            else if (e.key === 'a') snake0.set_direction(3);
+            else if (e.key === 'ArrowUp') snake1.set_direction(0);
+            else if (e.key === 'ArrowRight') snake1.set_direction(1);
+            else if (e.key === 'ArrowDown') snake1.set_direction(2);
+            else if (e.key === 'ArrowLeft') snake1.set_direction(3);
+        });
+    }
+    check_vaild(cell) {
+        for (const wall of this.walls) {
+            if (wall.r === cell.r && wall.c === cell.c)
+            return false;
+        }
+
+        for (const snake of this.snakes) {
+            let k = snake.cells.length;
+            if (!snake.check_tail_increasing) {
+                k -- ;
+            }
+
+            for (let i = 0; i < k; i ++ ) {
+                if (snake.cells[i].r === cell.r && snake.cells[i].c === cell.c)
+                return false;
+            }
+        }
+        return true;
+    }
     updated() {
         this.updated_size();
+        if (this.check_ready()) {
+            this.next_step();
+        }
         this.render();
+    }
+    next_step() {
+        for (const snake of this.snakes) {
+            snake.next_step();
+        }
     }
     render() {
         const color_even = "#AAD751", color_odd = "#A2D149";
