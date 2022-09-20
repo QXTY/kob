437dc180d469b918aaab69e0dc1f47e03cde9ab2
yisheng wang <468539746@qq.com>
2022-09-20 16:41:33 +0800

    backend start1 mysql

---
+++  backend/kob/src/main/java/com/qxty/kob/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.qxty.kob.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.qxty.kob.pojo.User;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper extends BaseMapper<User> {
+}
---  backend/kob/src/test/java/com/qxty/kob/KobApplicationTests.java
+++  backend/kob/src/test/java/com/qxty/kob/KobApplicationTests.java
@@ -2,12 +2,15 @@ package com.qxty.kob;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 class KobApplicationTests {
 
     @Test
     void contextLoads() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        System.out.println(bCryptPasswordEncoder.encode("qxty"));
     }
 
 }
